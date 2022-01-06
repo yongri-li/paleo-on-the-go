62e83db2c714535f3bd3c5262296097578760a79
Marc Wallis <marc@scoutside.com>
2022-01-06 10:33:52 -0800

    test: text compile and commit and push up to github repo

--- src/widgets/vue/portal/components/shipments/cShipmentsBox.vue
+++ src/widgets/vue/portal/components/shipments/cShipmentsBox.vue
@@ -3,28 +3,32 @@
     <div class="c-shipmentsBox__flex">
       <div class="c-shipmentsBox__flexSmall">
         <div class="c-shipmentsBox__shipping">
-          <span class="c-shipmentsBox__shippingCharges u-block"
+          <span
+            class="c-shipmentsBox__shippingCharges u-block"
             v-html="'this box charges'"
           />
-          <c-h class="c-shipmentsBox__shippingDate"
+          <c-h
+            class="c-shipmentsBox__shippingDate"
             tag="time"
             level="1"
             :text="'Mar 22'"
           />
-          <c-button class="c-shipments__shippingButton"
+          <c-button
+            class="c-shipments__shippingButton"
             :modifiers="['isDefault', 'isPrimary']"
             :text="'ship it asap'"
           />
-          <span class="c-shipmentsBox__shippingTo u-block"
+          <span
+            class="c-shipmentsBox__shippingTo u-block"
             v-html="'Ships To'"
           />
-          <c-button class="c-shipmentsBox__shippingAddress u-block"
+          <c-button
+            class="c-shipmentsBox__shippingAddress u-block"
             :text="'17 Princess Street'"
           />
         </div>
       </div>
-      <div class="c-shipmentsBox__flexLarge">
-      </div>
+      <div class="c-shipmentsBox__flexLarge"></div>
     </div>
   </div>
 </template>
@@ -41,73 +45,74 @@ export default {
     },
     modifiers: {
       type: Array,
-      default: () => ([])
+      default: () => []
     }
   },
-  components: { 
-    cH, cButton
+  components: {
+    cH,
+    cButton
   }
 }
 </script>
 
 <style lang="scss">
-  .c-shipmentsBox {
-    @include shadow-card;
-    padding: 15px 20px 20px;
-    background-color: $color-white;
-    border-radius: 3px;
-    &:not(:last-child) {
-      margin-bottom: 40px;
-    }
+.c-shipmentsBox {
+  @include shadow-card;
+  padding: 15px 20px 20px;
+  background-color: $color-white;
+  border-radius: 3px;
+  &:not(:last-child) {
+    margin-bottom: 40px;
   }
-  .c-shipmentsBox__flex {
-    @include flex($direction: column);
-    @include media-tablet-up {
-      @include flex($justify: space-between, $align: center, $wrap: nowrap);
-    }
+}
+.c-shipmentsBox__flex {
+  @include flex($direction: column);
+  @include media-tablet-up {
+    @include flex($justify: space-between, $align: center, $wrap: nowrap);
   }
-  .c-shipmentsBox__flexSmall {
-    width: 100%;
-    @include media-tablet-up {
-      max-width: 210px;
-    }
-    @include media-desktop-up {
-      max-width: 250px;
-    }
+}
+.c-shipmentsBox__flexSmall {
+  width: 100%;
+  @include media-tablet-up {
+    max-width: 210px;
   }
-  .c-shipmentsBox__flexLarge {
-    @include media-tablet-up {
-      max-width: calc(100% - 225px);
-    }
-    @include media-desktop-up {
-      max-width: calc(100% - 280px);
-    }
+  @include media-desktop-up {
+    max-width: 250px;
   }
-  .c-shipmentBox__shipping {
-    width: 100%;
+}
+.c-shipmentsBox__flexLarge {
+  @include media-tablet-up {
+    max-width: calc(100% - 225px);
   }
-  .c-shipmentsBox__shippingCharges {
-    @include text-heading;
-    font-size: 14px;
-    letter-spacing: 2px;
-    text-transform: uppercase;
+  @include media-desktop-up {
+    max-width: calc(100% - 280px);
   }
-  .c-shipmentsBox__shippingDate {
-    margin-bottom: 15px;
-    color: $color-secondary;
-    font-size: 50px;
-    line-height: 1;
-    text-transform: uppercase;
-  }
-  .c-shipments__shippingButton {
-    margin-bottom: 15px;
-  }
-  .c-shipments__shippingTo,
-  .c-shipments__shippingAddress {
-    @include text-heading;
-    font-size: 18px;
-  }
-  .c-shipments__shippingAddress {
-    color: $color-secondary;
-  }
+}
+.c-shipmentBox__shipping {
+  width: 100%;
+}
+.c-shipmentsBox__shippingCharges {
+  @include text-heading;
+  font-size: 14px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+}
+.c-shipmentsBox__shippingDate {
+  margin-bottom: 15px;
+  color: $color-secondary;
+  font-size: 50px;
+  line-height: 1;
+  text-transform: uppercase;
+}
+.c-shipments__shippingButton {
+  margin-bottom: 15px;
+}
+.c-shipments__shippingTo,
+.c-shipments__shippingAddress {
+  @include text-heading;
+  font-size: 18px;
+}
+.c-shipments__shippingAddress {
+  color: $color-secondary;
+}
 </style>
