2b57ae36bd0623217cd61e01adf3390a0bc3734d
Marc Wallis <marc@scoutside.com>
2022-02-17 22:29:36 -0800

    new content and styling for homepage, and global header and footer

--- src/scripts/sections/theme/theme.js
+++ src/scripts/sections/theme/theme.js
@@ -1,1 +1,24 @@
-import '@styles/index.scss';
+import '@styles/index.scss'
+import { tns } from 'tiny-slider/src/tiny-slider'
+
+if (document.querySelector('[data-tile-slider]')) {
+	const copyTiles = tns({
+		container: '[data-tile-slider]',
+		items: 1,
+		slideBy: 1,
+		autoplay: true,
+		controls: false,
+		autoplayButtonOutput: false
+	})
+}
+
+if (document.querySelector('[data-testimony-slider]')) {
+	const sliders = tns({
+		container: '[data-testimony-slider]',
+		items: 1,
+		slideBy: 1,
+		autoplay: false,
+		controls: false,
+		autoplayButtonOutput: false
+	})
+}
