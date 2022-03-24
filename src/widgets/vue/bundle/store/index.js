11a952c9ee3dbb537a63801a696563423c808601
davidsekielyk <david.sekielyk@gm2dev.com>
2022-03-24 18:39:22 -0300

    feat: refactor vuexpersistence
    feat: add reducer to choose what items from state keep persistence

--- src/widgets/vue/bundle/store/index.js
+++ src/widgets/vue/bundle/store/index.js
@@ -4,12 +4,19 @@ import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const persist = new VuexPersistence({
+  reducer: (state) => ({
+    cart: state.cart,
+    sizes: state.sizes
+  })
+})
+
 const store = new Vuex.Store({
   state: require('./_state').default,
   getters: require('./_getters').default,
   mutations: require('./_mutations').default,
   actions: require('./_actions').default,
-  plugins: [new VuexPersistence().plugin]
+  plugins: [persist.plugin]
 })
 
 export default store
