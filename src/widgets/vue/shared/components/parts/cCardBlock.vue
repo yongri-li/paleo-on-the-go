f201a82020eec07c5fa2855e6b4b3b0821e295e3
davidsekielyk <david.sekielyk@gm2dev.com>
2022-04-29 15:44:25 -0300

    feat: add page realfood

---
+++ src/widgets/vue/shared/components/parts/cCardBlock.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card-block">
+    <img
+      :src="imgCard"
+      :alt="header"
+      class="card-block__img"
+    />
+    <c-rich-text
+      :header="header"
+      :header-class="headerClass"
+      :body="body"
+      class="card-block__rich-text"
+    />
+  </div>
+</template>
+
+<script>
+import cRichText from '../core/cRichText.vue'
+
+export default {
+  components: {
+    cRichText
+  },
+  props: {
+    imgCard: {
+      type: String,
+      required: true
+    },
+    header: {
+      type: String,
+      required: true
+    },
+    headerClass: {
+      type: String,
+      default: 'c-h1'
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-block {
+  &__rich-text {
+    padding: 1rem .5rem;
+  }
+}
+</style>
