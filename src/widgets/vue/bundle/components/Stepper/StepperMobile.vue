3fb2eb9ace83dd6e6128b732db50f49947da7db9
Tyler Patton <jamestylerpatton@gmail.com>
2022-05-11 20:21:55 -0700

    style: bab step navigation tablet styles

--- src/widgets/vue/bundle/components/Stepper/StepperMobile.vue
+++ src/widgets/vue/bundle/components/Stepper/StepperMobile.vue
@@ -2,8 +2,23 @@
   <div class="sp">
     <div class="sp__item">
       <div class="sp__item--back" @click="backRouter(step)">
-        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M6.85355 1.15976C6.65829 0.946747 6.34171 0.946747 6.14645 1.15976L1.14645 6.61431C0.951185 6.82732 0.951185 7.17268 1.14645 7.38569L6.14645 12.8402C6.34171 13.0533 6.65829 13.0533 6.85355 12.8402C7.04881 12.6272 7.04881 12.2819 6.85355 12.0689L2.20711 7L6.85355 1.93115C7.04882 1.71814 7.04882 1.37277 6.85355 1.15976Z" fill="#231F20" stroke="#231F20" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg
+          width="8"
+          height="14"
+          viewBox="0 0 8 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M6.85355 1.15976C6.65829 0.946747 6.34171 0.946747 6.14645 1.15976L1.14645 6.61431C0.951185 6.82732 0.951185 7.17268 1.14645 7.38569L6.14645 12.8402C6.34171 13.0533 6.65829 13.0533 6.85355 12.8402C7.04881 12.6272 7.04881 12.2819 6.85355 12.0689L2.20711 7L6.85355 1.93115C7.04882 1.71814 7.04882 1.37277 6.85355 1.15976Z"
+            fill="#231F20"
+            stroke="#231F20"
+            stroke-width="0.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </svg>
       </div>
       {{ step.name }}
@@ -12,54 +27,52 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { changeRouter } from '../../utils'
+import { mapGetters } from "vuex";
+import { changeRouter } from "../../utils";
 
 export default {
   computed: {
-    ...mapGetters('mealcart', [
-      'getSizeSelected'
-    ]),
+    ...mapGetters("mealcart", ["getSizeSelected"]),
     step() {
-      const param = this.$route.params.box
-      if(param === 'subscription') {
+      const param = this.$route.params.box;
+      if (param === "subscription") {
         return {
           name: `Choose ${this.getSizeSelected.number_size} Items To Continue`,
-          url: '/'
-        }
+          url: "/",
+        };
       }
-      if(param === 'onetime') {
+      if (param === "onetime") {
         return {
-          name: '8 Item Minimum',
-          url: '/'
-        }
+          name: "8 Item Minimum",
+          url: "/",
+        };
       }
-      if(param === 'addons') {
+      if (param === "addons") {
         return {
-          name: 'One Time Add Ons',
-          param: 'subscription'
-        }
+          name: "One Time Add Ons",
+          param: "subscription",
+        };
       }
-      return 'Review'
-    }
+      return "Review";
+    },
   },
   methods: {
     backRouter(step) {
-      if(step.url) {
-        window.location = '/'
+      if (step.url) {
+        window.location = "/";
       }
-      if(step.param) {
-        this.$router.go(-1)
+      if (step.param) {
+        this.$router.push(`/${step.param}`);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .sp {
-  background-color: #FBFAF4;
-  margin: 0 -10px .7rem;
+  background-color: #fbfaf4;
+  margin: 0 -10px 0.7rem;
 
   &__item {
     text-align: center;
@@ -68,10 +81,9 @@ export default {
 
     &--back {
       position: absolute;
-      left: .5rem;
-      padding: 0 .5rem;
+      left: 0.5rem;
+      padding: 0 0.5rem;
     }
   }
-
 }
 </style>
