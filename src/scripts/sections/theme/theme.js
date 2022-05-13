90eeecd5026949ab8a056d4b6e77a9d7c4f3210d
Marc Wallis <marc@scoutside.com>
2022-05-13 11:58:20 -0700

    footer email fixes and get started header banner button fix for box size

--- src/scripts/sections/theme/theme.js
+++ src/scripts/sections/theme/theme.js
@@ -5,6 +5,7 @@ const windowPath = window.location.pathname
 // if (windowPath === '/account') window.location.href = '/account#/shipments/'
 const isHomepage = document.querySelector('.o-homepage')
 const getStartedBtns = document.querySelectorAll('[data-get-started]')
+const startBoxBab = document.querySelectorAll('[data-start-box]')
 const boxTabs = document.querySelectorAll('.js--planClk')
 const logoutLinks = document.querySelectorAll('[data-logout-link]')
 
@@ -17,6 +18,9 @@ const sendBoxInfo = btn => {
 	window.location.href = '/pages/bundle/#/subscription'
 }
 getStartedBtns.forEach(btn => btn.addEventListener('click', () => sendBoxInfo(btn)))
+startBoxBab.forEach(btn =>
+	btn.addEventListener('click', () => (location.href = '/pages/bundle/#/subscription'))
+)
 if (isHomepage) boxTabs.forEach(box => box.addEventListener('click', () => sendBoxInfo(box)))
 
 logoutLinks.forEach(lnk =>
