acfae9ff9e46fc8c22916c8325898ef81f12842f
Tyler Patton <jamestylerpatton@gmail.com>
2022-05-12 18:33:44 -0700

    style: cart - update subtotal column styles

--- src/widgets/vue/cart/Cart.vue
+++ src/widgets/vue/cart/Cart.vue
@@ -2,44 +2,37 @@
   <div class="page o-containerLarge">
     <summary-component class="content-summary" />
     <div v-if="hasVisibleCartItems" class="content-box-cart">
-      <your-box v-if="cartItems.box.length"/>
-      <your-cart v-if="visibleCartItems.length"/>
+      <your-box v-if="cartItems.box.length" />
+      <your-cart v-if="visibleCartItems.length" />
     </div>
-    <div v-else>
-      Cart is Empty
-    </div>
+    <div v-else>Cart is Empty</div>
   </div>
 </template>
 
 <script>
-import SummaryComponent from './components/Summary/Summary.vue'
-import YourBox from './components/YourBox/YourBox.vue'
-import YourCart from './components/YourCart/YourCart.vue'
+import SummaryComponent from "./components/Summary/Summary.vue";
+import YourBox from "./components/YourBox/YourBox.vue";
+import YourCart from "./components/YourCart/YourCart.vue";
 
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   components: {
     SummaryComponent,
     YourBox,
-    YourCart
+    YourCart,
   },
   computed: {
-    ...mapState('cartdrawer',[
-      'cartItems',
-      'sizeSelected'
-    ]),
-    ...mapState('ui',[
-      'test'
-    ]),
+    ...mapState("cartdrawer", ["cartItems", "sizeSelected"]),
+    ...mapState("ui", ["test"]),
     visibleCartItems() {
-      return this.cartItems.general.filter(item => item.hide !== true)
+      return this.cartItems.general.filter((item) => item.hide !== true);
     },
     hasVisibleCartItems() {
-      return this.visibleCartItems.length || this.cartItems.box.length
-    }
-  }
-}
+      return this.visibleCartItems.length || this.cartItems.box.length;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -47,17 +40,34 @@ export default {
   margin-top: 1rem;
 
   @include media-tablet-up {
-    width: 80%;
     max-width: 1400px;
     margin-top: 3rem;
-    @include flex($justify: space-between, $align: flex-start, $direction: row-reverse);
+    @include flex(
+      $justify: space-between,
+      $align: flex-start,
+      $direction: row-reverse,
+      $wrap: nowrap
+    );
 
     .content-box-cart {
-      width: 68%;
+      padding-right: 1.25rem;
     }
 
     .content-summary {
-      width: 28%;
+      width: 280px;
+      flex-shrink: 0;
+    }
+  }
+
+  @include media-desktop-up {
+    width: 80%;
+
+    .content-box-cart {
+      padding-right: 2.63rem;
+    }
+
+    .content-summary {
+      width: 310px;
     }
   }
 }
--- src/widgets/vue/cart/components/Summary/SummarySubtotal.vue
+++ src/widgets/vue/cart/components/Summary/SummarySubtotal.vue
@@ -24,58 +24,70 @@
           v-model="agree"
           class="subtotal__agree--check"
         />
-        <label for="agreecheck" class="subtotal__agree--label" :class="{ agree: agree }">
+        <label
+          for="agreecheck"
+          class="subtotal__agree--label"
+          :class="{ agree: agree }"
+        >
           I agree with the
         </label>
-        <a href="#" target="_blank" class="subtotal__agree--redirect"> Shipping Terms and Conditions </a>
+        <a href="#" target="_blank" class="subtotal__agree--redirect">
+          Shipping Terms and Conditions
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-import { formatPrice, formatPriceDollars } from '@shared/utils'
-import cButton from '@shared/components/core/cButton.vue'
+import { mapState, mapGetters } from "vuex";
+import { formatPrice, formatPriceDollars } from "@shared/utils";
+import cButton from "@shared/components/core/cButton.vue";
 
 export default {
   components: {
-    cButton
+    cButton,
   },
   data() {
     return {
       agree: false,
-      loading: false
-    }
+      loading: false,
+    };
   },
   components: { cButton },
   computed: {
-    ...mapState('ui', ['settings']),
-    ...mapState('cartdrawer', ['cartItems', 'sizeSelected']),
-    ...mapState('frequency', ['frequencySelected']),
-    ...mapGetters('cartdrawer', ['getBoxPrices', 'getAddonPrices', 'getGeneralPrices']),
+    ...mapState("ui", ["settings"]),
+    ...mapState("cartdrawer", ["cartItems", "sizeSelected"]),
+    ...mapState("frequency", ["frequencySelected"]),
+    ...mapGetters("cartdrawer", [
+      "getBoxPrices",
+      "getAddonPrices",
+      "getGeneralPrices",
+    ]),
     finalSubtotal() {
       return formatPriceDollars(
-        this.getBoxPrices.final + this.getAddonPrices / 100 + this.getGeneralPrices / 100
-      )
+        this.getBoxPrices.final +
+          this.getAddonPrices / 100 +
+          this.getGeneralPrices / 100
+      );
     },
     items() {
-      const box = this.getItemForCart('box')
-      const general = this.getItemForCart('general')
-      return [...box, ...general]
-    }
+      const box = this.getItemForCart("box");
+      const general = this.getItemForCart("general");
+      return [...box, ...general];
+    },
   },
   methods: {
     getItemForCart(itemType) {
       const subsprops = {
         shipping_interval_frequency: this.frequencySelected.week,
-        shipping_interval_unit_type: 'week'
-      }
+        shipping_interval_unit_type: "week",
+      };
 
-      return this.cartItems[itemType].map(item => {
-        const isSubscription = item.order_type === 'subscription'
-        const otherProps = isSubscription ? subsprops : {}
-        const discount = isSubscription ? this.sizeSelected.discount / 100 : 0
+      return this.cartItems[itemType].map((item) => {
+        const isSubscription = item.order_type === "subscription";
+        const otherProps = isSubscription ? subsprops : {};
+        const discount = isSubscription ? this.sizeSelected.discount / 100 : 0;
 
         return {
           id: item.varId ? item.varId : item.variants[0].id,
@@ -85,15 +97,15 @@ export default {
           price: item.varPrice ? item.varPrice / 100 : item.price / 100,
           // price: item.price * (1 - discount),
           properties: {
-            _onetime: item.order_type === 'onetime',
-            _subscription: item.order_type === 'subscription',
-            _addons: item.order_type === 'addons',
-            _general: item.order_type === 'general',
+            _onetime: item.order_type === "onetime",
+            _subscription: item.order_type === "subscription",
+            _addons: item.order_type === "addons",
+            _general: item.order_type === "general",
             _subtitle: item.subtitle,
-            ...otherProps
-          }
-        }
-      })
+            ...otherProps,
+          },
+        };
+      });
     },
     buildCartData() {
       return {
@@ -101,77 +113,80 @@ export default {
         note_attributes: [],
         attributes: {},
         note: null,
-        email: null
-      }
+        email: null,
+      };
     },
     async sendProductToCart(cartData) {
       // clean cart
-      const clearRequest = await fetch('/cart/clear.js', {
-        method: 'POST',
+      const clearRequest = await fetch("/cart/clear.js", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+          "Content-Type": "application/json",
+        },
+      });
 
       // add product to cart
-      const addRequest = await fetch('/cart/add.js', {
-        method: 'POST',
+      const addRequest = await fetch("/cart/add.js", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(cartData)
-      })
+        body: JSON.stringify(cartData),
+      });
 
-      return addRequest
+      return addRequest;
     },
     async getTokenFromCart() {
-      const tokenRequest = await fetch('/cart.js')
-      const tokenJson = await tokenRequest.json()
-      return tokenJson.token
+      const tokenRequest = await fetch("/cart.js");
+      const tokenJson = await tokenRequest.json();
+      return tokenJson.token;
     },
     async checkout() {
-      this.loading = true
+      this.loading = true;
       // create cartData
-      const cartData = this.buildCartData()
+      const cartData = this.buildCartData();
 
       // send data to cart (clear & add)
-      const addRequest = await this.sendProductToCart(cartData)
-      console.log(addRequest)
+      const addRequest = await this.sendProductToCart(cartData);
+      console.log(addRequest);
       if (addRequest.status !== 200) {
         // throw error
         // show error msj
-        return
+        return;
       }
 
       // get cart hash
-      const hashRequest = await fetch('/cart?view=hash', {
-        method: 'GET',
+      const hashRequest = await fetch("/cart?view=hash", {
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        }
-      })
-      const hashJson = await hashRequest.json()
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const hashJson = await hashRequest.json();
 
       // get token
-      const token = await this.getTokenFromCart()
+      const token = await this.getTokenFromCart();
 
-      if (this.sizeSelected.order_type === 'subscription') {
-        const appRequest = await fetch('https://paleo-custom-app.herokuapp.com/plan/checkout', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-paleo-key': window.Scoutside.api.custom_app_key
-          },
-          body: JSON.stringify(hashJson)
-        })
-        const appResponse = await appRequest.json()
-        const domain = 'paleo-on-the-go-sandbox.myshopify.com'
-        window.location = `https://checkout.rechargeapps.com/r/checkout/${appResponse.token}?myshopify_domain=${domain}`
-      } else window.location = '/checkout'
-    }
-  }
-}
+      if (this.sizeSelected.order_type === "subscription") {
+        const appRequest = await fetch(
+          "https://paleo-custom-app.herokuapp.com/plan/checkout",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "x-paleo-key": window.Scoutside.api.custom_app_key,
+            },
+            body: JSON.stringify(hashJson),
+          }
+        );
+        const appResponse = await appRequest.json();
+        const domain = "paleo-on-the-go-sandbox.myshopify.com";
+        window.location = `https://checkout.rechargeapps.com/r/checkout/${appResponse.token}?myshopify_domain=${domain}`;
+      } else window.location = "/checkout";
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -184,21 +199,21 @@ export default {
 
   &__title {
     text-align: center;
-    font-size: 1.7rem;
+    font-size: 1.5rem;
     font-weight: 500;
   }
 
   &__price {
     font-family: $font-heading;
     text-align: center;
-    font-size: 3.5rem;
+    font-size: 2.88rem;
     margin: 0.8rem 0;
   }
 
   &__checkout {
     width: 100%;
     max-width: 100%;
-    font-size: 1.3rem;
+    font-size: 1.13rem;
     font-weight: 500;
     margin-bottom: 1.5rem;
   }
@@ -210,11 +225,11 @@ export default {
   }
 
   &__msg {
-    font-size: 1.1rem;
+    font-size: 0.88rem;
   }
 
   &__agree {
-    font-size: 0.8rem;
+    font-size: 0.63rem;
     margin: 0.5rem 0;
     @include flex();
 
@@ -226,7 +241,7 @@ export default {
       @include flex();
 
       &::before {
-        content: '';
+        content: "";
         display: inline-block;
         margin-right: 3px;
         width: 13px;
@@ -238,7 +253,7 @@ export default {
     }
     &--label.agree {
       &::before {
-        content: '✓';
+        content: "✓";
         color: $color-primary;
         background-color: $color-black;
         border: 1px solid $color-black;
