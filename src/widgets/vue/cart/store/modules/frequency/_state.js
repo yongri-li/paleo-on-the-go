b0ff10c78c5f32f0a882fde4d5570982d9e6a474
Marc Wallis <marc@scoutside.com>
2022-05-26 11:31:11 -0700

    style: update weeks copy on cart, adding white and overlay backgrounds for bottom hero

--- src/widgets/vue/cart/store/modules/frequency/_state.js
+++ src/widgets/vue/cart/store/modules/frequency/_state.js
@@ -1,29 +1,29 @@
 const state = {
   frequencies: [
     {
-      label: 'Ships Every 2 Week',
-      labelOption: '2 Week',
+      label: 'Ships Every 2 Weeks',
+      labelOption: '2 Weeks',
       week: 2
     },
     {
-      label: 'Ships Every 4 Week',
-      labelOption: '4 Week',
+      label: 'Ships Every 4 Weeks',
+      labelOption: '4 Weeks',
       week: 4
     },
     {
-      label: 'Ships Every 6 Week',
-      labelOption: '6 Week',
+      label: 'Ships Every 6 Weeks',
+      labelOption: '6 Weeks',
       week: 6
     },
     {
-      label: 'Ships Every 8 Week',
-      labelOption: '8 Week',
+      label: 'Ships Every 8 Weeks',
+      labelOption: '8 Weeks',
       week: 8
-    },
+    }
   ],
   frequencySelected: {
-    label: 'Ships Every 2 Week',
-    labelOption: '2 Week',
+    label: 'Ships Every 2 Weeks',
+    labelOption: '2 Weeks',
     week: 2
   }
 }
